94f57ec8c6e07ff152c1ff4def74ad3a3d5b7cc8
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-02-15 15:11:02 -0500

    Created Pet Page
    -Created Pet Page to show additional details for selected pet
    -Imported react-router-dom to create routes for the Pet Page

---
+++ faf/src/PetPage/PetPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import App from "../App";
+import { useState, useEffect } from "react";
+
+const PetPage = () => {
+  return <div></div>;
+};
+
+export default PetPage;
--- faf/src/index.js
+++ faf/src/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PetPage from "./PetPage/PetPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route exact path="/petinfo" element={<PetPage />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
