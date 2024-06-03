6be16b48984acf3b7de304a913c26bbf04f7b660
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-06-02 20:39:45 -0400

    Update App.js

--- faf/src/App.js
+++ faf/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import { token } from "./Petfinder_API/Key_Generator";
 import makeRequest from "./Petfinder_API/Key_Generator";
@@ -9,15 +9,17 @@ function App() {
   const [animalList, setAnimalList] = useState([]);
   const [selectedAnimal, setSelectedAnimal] = useState("");
 
-
   // const token =
   //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJNZ2JNWU9mS2dmemZyTzZ4STZWdXY4eVVncGV5c2lxcENkd1d3azRNdnZ1b0o1U1hSeiIsImp0aSI6ImIwODFiNTQ5OGQ5ODU0ZmIwNTVkMTYxNjI5ODdiZDViZmQ4NWVmN2NhZWZkNTI0ZmY4OTcxZGQyMjk0OGY4MWYwZTc4Zjc0NjI2ZTNhZTk3IiwiaWF0IjoxNzA0ODI1OTU3LCJuYmYiOjE3MDQ4MjU5NTcsImV4cCI6MTcwNDgyOTU1Nywic3ViIjoiIiwic2NvcGVzIjpbXX0.z3GaQRfJexnXCTlvuTKJ89MOUMw-ieJR5vtPdl0DQdxO04FRt_rdS_qTlxhBxhgtvrEqXvGmsCPzOi3qJomZ5fkyXycvlkJqtzgDCTZVve1oQgDx8vNQOw_copCMdC6E61Rdwr8zS3gpoRzWMxTGVUi5uylfN5tbuKBvEAHK9bbLFuvbaT8gQ8E-woT42bcdJ5gtRw_3b5uhoqEx4FrR_PlN5zwroLg2mpEKndcJ_e3PVfoBj71s5VQlalT4jqfYwkPY5w4AYcrKnEe6JBetG_IEPKNIynQYOaNyw4Ym3QEs_xVyoQdgPR8QL9W5kDkx3q8ijNrpSFn9JORGIlsUSQ";
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+  const config = useMemo(
+    () => ({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+    [token]
+  );
 
   function catButton() {
     setSelectedAnimal((selectedAnimal) => "cat");
