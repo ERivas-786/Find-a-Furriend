9c3bf44b10bd186aa2f35eff02170bf8d4bee69e
Emanuel Rivas <Emanuelrivasdavid@gmail.com>
2024-06-03 14:15:30 -0400

    Revert "Updated yml"
    This reverts commit 7b91c042f0360c2c78b3d218d01c37d31f9cc333.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.js$': 'babel-jest'
-  },
-  testEnvironment: 'jsdom',
-  moduleFileExtensions: ['js', 'jsx'],
-  transformIgnorePatterns: ['/node_modules/'],
-};
